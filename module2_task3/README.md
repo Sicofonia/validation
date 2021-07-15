c4fdece69865c4d32ff5af99bf5cf8765421c759
Sicofonia <sicofonia@hotmail.com>
2021-07-15 10:06:21 -0400

    Completed module 2 task 3

---
+++ module2_task3/README.md
@@ -0,0 +1,9 @@
+## Lifecycle
+1. Make build to build API
+2. Make run to run the API
+3. Make test to run both integration and unit tests
+4. Make stop to kill it
+5. Make clean to clear binaries
+6. Make lint to run static analysis
+7. Make unit-tests to run unit tests
+8. Make integration-tests to run integration tests
---
+++ module2_task3/main.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+)
+
+func main() {
+	httpAddr := "0.0.0.0:9999"
+	if port := os.Getenv("PORT"); port != "" {
+		httpAddr = "0.0.0.0:" + port
+	}
+	fmt.Println("HTTP server listening on ", httpAddr)
+
+	log.Fatal(http.ListenAndServe(httpAddr, setupRouter()))
+}
+
+func setupRouter() *mux.Router {
+	r := mux.NewRouter()
+
+	r.HandleFunc("/health", HealthCheckHandler).Methods("GET")
+	r.HandleFunc("/hello", HelloHandler).Methods("GET")
+	return r
+}
+
+func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("HIT: healthcheck")
+	_, _ = io.WriteString(w, "ALIVE")
+}
+
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+	queryParams := r.URL.Query()
+	nameParams := queryParams["name"]
+	var name string
+	switch len(nameParams) {
+	case 0:
+		name = ""
+	case 1:
+		name = nameParams[0]
+	default:
+		name = nameParams[len(nameParams)-1]
+	}
+	if name == "" {
+		name = "there"
+	}
+	_, _ = io.WriteString(w, fmt.Sprintf("Hello %s!", name))
+	fmt.Printf("HIT: hello handler with name %s \n", name)
+}
